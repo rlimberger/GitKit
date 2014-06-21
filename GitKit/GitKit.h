4db6a3ad4fea58187833b14d46fd25ecc191a284
Russell Richardson <Russell@KronosAD.com>
2014-06-21 12:46:35 -0400

    Initial Commit

---
+++ GitKit/GitKit.h
@@ -0,0 +1,19 @@
+//
+//  GitKit.h
+//  GitKit
+//
+//  Created by Russell Richardson on 6/21/14.
+//  Copyright (c) 2014 Russell Richardson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GitKit.
+FOUNDATION_EXPORT double GitKitVersionNumber;
+
+//! Project version string for GitKit.
+FOUNDATION_EXPORT const unsigned char GitKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitKit/PublicHeader.h>
+
+
